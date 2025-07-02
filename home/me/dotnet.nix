ce42c9cf8bf6a78c1da801686ea7f8eb0c85c780
Jonathan Peel <me@jonathanpeel.co.za>
2025-07-02 21:06:57 +0300

    .

--- home/me/dotnet.nix
+++ home/me/dotnet.nix
@@ -1,10 +1,21 @@
 { config, lib, pkgs, ... }:
 with pkgs;
 let
-  dotnet-combined = (with pkgs.dotnetCorePackages; combinePackages [ dotnet_10.sdk dotnet_9.sdk dotnet_8.sdk ]);
+  dotnet-combined =
+    (with pkgs.dotnetCorePackages;
+      combinePackages [
+        sdk_10_0-bin
+        sdk_9_0
+        sdk_8_0
+      ]);
 
 in
 {
-  sessionVariables = { DOTNET_ROOT = "${dotnet-combined}"; };
-  home.packages = with pkgs [ dotnet-combined ];
+  home = {
+    packages = with pkgs; [
+      dotnet-combined
+      jetbrains.rider
+    ];
+    sessionVariables = { DOTNET_ROOT = "${dotnet-combined}"; };
+  };
 }
